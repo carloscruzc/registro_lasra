fbc4685177379cfe504812762728a2e85d67c45d
AngelMunguia1 <angel.munguia@grupolahe.com>
2022-08-02 14:10:13 -0500

    zxc

--- backend/App/views/account_all.php
+++ backend/App/views/account_all.php
@@ -185,7 +185,7 @@
                                     </div>
 
                                     <div class="col-lg-4 col-12">
-                                        <label class="form-label mt-4">Categoria </label>
+                                        <label class="form-label mt-4">Categoría </label>
                                         
                                             
                                           <input class="form-control" id="categoria" maxlength="149"  name="categoria" data-color="dark" type="text" value="<?= $categoria ?>" placeholder="Ej: México" readonly/> 

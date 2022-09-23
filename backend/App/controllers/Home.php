14297fbd3b07b8e85285f9f5b9bc1761a2445b21
Gerson Velasco <gvelassco@gmail.com>
2022-09-23 16:44:13 -0500

    ajustes

--- backend/App/controllers/Home.php
+++ backend/App/controllers/Home.php
@@ -429,7 +429,7 @@ html;
             if($datos_user_anualidad){
                 $comprobante .= <<<html
                 <div class="col-6 m-auto m-md-0 col-lg-3 col-md-4 my-md-3 mt-4">
-                    <a href="#">
+                    <a href="/ComprobantePago/">
                         <div class="card card-link btn-menu-home m-auto" style="background-image: url(/img/SMNP_Iconos/Web3.png); background-size: contain; background-repeat: no-repeat;">
                             <div class="card-body mt-md-3 text-center content-card-home">
                                 <div class="col-12 text-center">

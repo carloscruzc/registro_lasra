7cf302c1fbab45954238762ba103f87d9717b288
Gerson Velasco <gvelassco@gmail.com>
2022-10-03 16:12:46 -0500

    ajuste

--- backend/App/controllers/Home.php
+++ backend/App/controllers/Home.php
@@ -204,13 +204,8 @@ html;
             }
             else if($datos_estudiante){
                 $comprobante .= <<<html
-<<<<<<< HEAD
                 <div class="col-6 m-auto m-md-0 col-lg-3 col-md-4 my-md-3 mt-4">
                     <a href="/ComprobanteEstudiante/">
-=======
-                <div class="col-6 m-auto m-md-0 col-lg-4 col-md-4 my-md-4 mt-4">
-                    <a href="#">
->>>>>>> 0a032c0957cd106da52d4bf6f2e9d4b2e67093a4
                         <div class="card card-link btn-menu-home m-auto" style="background-image: url(/img/SMNP_Iconos/Web3.png); background-size: contain; background-repeat: no-repeat;">
                             <div class="card-body mt-md-4 text-center content-card-home">
                                 <div class="col-12 text-center">
